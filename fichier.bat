e93712d87d0c824e10eea6768c86aaef578b51e2
Bks213 <47408814+Bks213@users.noreply.github.com>
2022-12-25 00:40:21 -0500

    Add files via upload

---
+++ fichier.bat
@@ -0,0 +1,1 @@
+powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Automatic logoff after 1 hour of inactivity','WARNING')}"
